655a0b05092f54547c85b1fc2588cdedac4a0ca6
lojt-cloud <264168469+lojt-cloud@users.noreply.github.com>
2026-07-30 09:39:43 +0200

    created outputs for modules

---
+++ modules/web-server/outputs.tf
@@ -0,0 +1,16 @@
+# modules/web-server/outputs.tf
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.this.id
+}
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.this.public_ip
+}
+
+output "private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = aws_instance.this.private_ip
+}
